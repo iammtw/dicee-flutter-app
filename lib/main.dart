28c3ec3908b81a5f12f0c57ecba138095b6e868f
MTW <muhammadtalhawaseem@gmail.com>
2021-03-29 22:37:40 +0500

    Complete

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: MyDice(),
+      ),
+    );
+  }
+}
+
+class MyDice extends StatefulWidget {
+  @override
+  _MyDiceState createState() => _MyDiceState();
+}
+
+class _MyDiceState extends State<MyDice> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void didFaceChange() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                this.didFaceChange();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                this.didFaceChange();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
